ed70cf36865e0703d23d9b84850fdc224edc1adf
Brigette <long.brigettee@gmail.com>
2024-04-16 14:24:29 -0700

    Create display for results
    Work in progress - ui currently shows which user wins when the game ends.

--- scratch.js
+++ scratch.js
@@ -1,41 +1,20 @@
 function gameBoard() {
-    const rows = 2; 
-    const columns = 2; 
-    let board = []; 
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = []; 
-        for (let j = 0; j < columns; j++) {
-            board[i].push(' '); 
-        }
-    }
-
+    //keep track of moves
     let moves = 0; 
     const totalMoves = () => {
         moves++;
         return moves;
     }
 
+    //control board activity 
     const visualBoard = document.querySelector('.board');
     visualBoard.addEventListener('click', (event) => {
         if (event.target.classList.contains('cell')) {
             toggleToken(event.target);
             checkWin();
-            // printBoard();
         } 
     })
-
-    // const printBoard = () => {
-    //     const cells = visualBoard.querySelectorAll('.cell');
-    //     cells.forEach(cell => {
-    //         let row = parseInt(cell.dataset.row); 
-    //         let col = parseInt(cell.dataset.column);
-    //         cell[row][col] = cell.textContent;  
-    //     })
-    // }
-
-    //keep track of turns played 
-    
 
     //cells listens for click, and assigns an x or o 
     const toggleToken = (cell) => {
@@ -51,56 +30,60 @@ function gameBoard() {
             return;
         }
     }
-
-    return {toggleToken}
 }
 
 
 function checkWin() {
     const cellArray = []
+    const displayRounds = document.querySelector('.countrounds');
 
     let cells = document.querySelectorAll('.cell');
     cells.forEach(cell => {
         cellArray.push(cell);
     })
 
+    const playerOne = {
+        token: 'X',
+        win: false,
+        winStatement: 'Player one wins!',
+        points: 0
+    }
+    const playerTwo = {
+        token: 'O',
+        win: false, 
+        winStatement: 'Player two wins!',
+        points: 0
+    }
+
     function findWinner() {
         //check rows
         if (cellArray[0].textContent !== '' && cellArray[1].textContent === cellArray[0].textContent && cellArray[2].textContent === cellArray[1].textContent) {
-            console.log(cellArray[0].textContent + ' wins');
-            return true;
+            return cellArray[0].textContent;
         }
         if (cellArray[3].textContent !== '' && cellArray[4].textContent === cellArray[3].textContent && cellArray[5].textContent === cellArray[4].textContent) {
-            console.log(cellArray[3].textContent + ' wins');
-            return true;
+            return cellArray[3].textContent;
         }
         if (cellArray[6].textContent !== '' && cellArray[7].textContent === cellArray[6].textContent && cellArray[8].textContent === cellArray[7].textContent) {
-            console.log(cellArray[6].textContent + ' wins');
-            return true;
+            return cellArray[6].textContent;
         }
 
         //check columns
         if (cellArray[0].textContent !== '' && cellArray[3].textContent === cellArray[0].textContent && cellArray[6].textContent === cellArray[3].textContent) {
-            console.log(cellArray[0].textContent + ' wins');
-            return true;
+            return cellArray[0].textContent;
         }
         if (cellArray[1].textContent !== '' && cellArray[4].textContent === cellArray[1].textContent && cellArray[7].textContent === cellArray[4].textContent) {
-            console.log(cellArray[1].textContent + ' wins');
-            return true;
+            return cellArray[1].textContent;
         }
         if (cellArray[2].textContent !== '' && cellArray[5].textContent === cellArray[2].textContent && cellArray[8].textContent === cellArray[5].textContent) {
-            console.log(cellArray[2].textContent + ' wins');
-            return true;
+            return cellArray[2].textContent;
         }
 
         //check diagonals
         if (cellArray[0].textContent !== '' && cellArray[4].textContent === cellArray[0].textContent && cellArray[8].textContent === cellArray[4].textContent) {
-            console.log(cellArray[0].textContent + ' wins');
-            return true;
+            return cellArray[0].textContent;
         }
         if (cellArray[2].textContent !== '' && cellArray[4].textContent === cellArray[2].textContent && cellArray[6].textContent === cellArray[4].textContent) {
-            console.log(cellArray[2].textContent + ' wins');
-            return true;
+            return cellArray[2].textContent;
         }
     
         return false;
@@ -115,20 +98,67 @@ function checkWin() {
                     return true;
                 } 
             }
-
             return false;
         }
 
         if (checkEmptyCells() === false) {
-            console.log('tie!')
+            console.log('tie!') 
         } else {
             return;
         }
     }
-}
 
-function displayResults() {
+    //update player properties and display results
+    const resultPanel = document.querySelector('.displayresults');
+    const playerOnePts = document.querySelector('.playeronepoints');
+    const playerTwoPts = document.querySelector('.playertwopoints');
 
+    if (findWinner() === playerOne.token) {
+        playerOne.win = true; 
+        playerOne.points++;
+        playerOnePts.textContent = 'Player One: ' + playerOne.points;
+        playerTwoPts.textContent = 'Player Two: ' + playerTwo.points;
+    
+        resultPanel.textContent = playerOne.winStatement;
+        } 
+
+    if (findWinner() === playerTwo.token) {
+        playerTwo.win = true; 
+        playerTwo.points++;
+        playerOnePts.textContent = 'Player One: ' + playerOne.points;
+        playerTwoPts.textContent = 'Player Two: ' + playerTwo.points;
+
+        resultPanel.textContent = playerTwo.winStatement;
+    }
+
+    //display rounds
+    let rounds = 0;
+    if (findWinner !== false || checkEmptyCells === false) {
+        rounds++;
+        displayRounds.innerText = 'Round ' + rounds;
+    }
+
+    //new round & game
+    newRoundBtn = document.querySelector('.newround')
+    newRoundBtn.addEventListener('click', () => {
+        newRound();
+    })
+
+
+    function newRound() {
+        if (playerOne.points < 5 && playerTwo.points < 5) {
+            playerOne.win = false; 
+            playerTwo.win = false;
+            cells.forEach(cell => cell.textContent = '');
+            resultPanel.textContent = '';
+        } 
+    }
+
+    function newGame() {
+        if (playerOne.points === 5 || playerTwo.points === 5) {
+            //
+        }
+    }
 }
 
 const initiateBoard = gameBoard();
